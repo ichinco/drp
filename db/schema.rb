753e52dd2308cd57ad3e8019bbe9e3e9653cc811
Francesco Fiordalisi <fiorda.f@gmail.com>
2013-09-28 16:19:26 -0400

    added password column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130917022653) do
+ActiveRecord::Schema.define(version: 20130928201631) do
 
   create_table "projects", force: true do |t|
     t.datetime "created_at"
@@ -23,9 +23,12 @@ ActiveRecord::Schema.define(version: 20130917022653) do
     t.datetime "updated_at"
   end
 
-  create_table "test_models", force: true do |t|
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,7 +1,21 @@
 require 'spec_helper'
 
 describe "Users" do
-    before { @user = Users.new name: " ", email: "user@example.com" }
-    subject { @user }
-    it { should respond_to ( :email ) }
+    describe "Name, email" do
+      before { @user = Users.new name: " ", email: "user@example.com",
+               password: 'abacadef' }
+      subject { @user }
+      it { should respond_to ( :email ) }
+      it { should respond_to :password }
+    end
+
+    describe "when password is not present" do
+       before do
+          @user = Users.new(
+             name: "Example User", email: "user@example.com",
+                password: 'abacadef')
+       end
+       subject{ @user }
+       it { should_not be_valid }
+    end 
 end
