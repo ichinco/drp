cd2711e8b289ea01054adc1cdce56897f892db35
Knight Fu <knight.w.fu@gmail.com>
2013-10-12 20:18:22 -0400

    modified gemfile and project

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,11 +3,14 @@
 # mentee are working on.
 #
 class Project < ActiveRecord::Base
-   #has_one :student
-   #has_one :mentor
-   #has_many :reference
-   #has_one :date
-   #has_one :state
+   has_one :student, :class_name => 'Users'
+   has_one :mentor, :class_name => 'Users'
+   has_one :project_state
+
+   validates :project_state, :presence => true
+   validates :student, :presence => true
+   validates :mentor, :presence => true
+   validates :title, :presence => true
    
    attr_accessor :title, :summary, :description
 end
---
+++ app/models/project_state.rb
@@ -0,0 +1,2 @@
+class ProjectState < ActiveRecord::Base
+end
