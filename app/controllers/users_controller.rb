343e6bc1bcc9e07aed52296c351779ad47dc1fb4
Francesco Fiordalisi <fiorda.f@gmail.com>
2014-01-22 20:28:09 -0500

    changed show user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
   def view
+    @users = Users.all
   end
 
   def show
+    #
+    # change roles_string
+    #
     @user  = Users.find(params[:id])
     @name  = "#{@user.first_name} #{@user.last_name}" 
     @email = @user.email
+    roles = ["admin"]
+    if @user.mentor_profile
+        roles << "mentor"
+    end
+    @roles_string = roles.join ", "
   end
 end
