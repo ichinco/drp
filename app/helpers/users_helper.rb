029907993c98c93728e82b6672d1f6a465ebe747
Francesco Fiordalisi <fiorda.f@gmail.com>
2014-01-26 09:46:35 -0500

    added gravatar size

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,6 +4,6 @@ module UsersHelper
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     name = "#{user.first_name} #{user.last_name}"
-    image_tag(gravatar_url, alt: name, class: "gravatar")
+    image_tag(gravatar_url, alt: name, class: "gravatar", size:"#{size}x#{size}")
   end
 end
